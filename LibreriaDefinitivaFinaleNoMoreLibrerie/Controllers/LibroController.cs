8eb396f7bd080a3b3ec89c8b7d5c91091ee9a6ab
Andrea Martino <andrea.martino@isisfacchinetti.ed.it>
2024-06-10 10:07:44 +0200

    Database Migration

--- LibreriaDefinitivaFinaleNoMoreLibrerie/Controllers/LibroController.cs
+++ LibreriaDefinitivaFinaleNoMoreLibrerie/Controllers/LibroController.cs
@@ -13,7 +13,7 @@ using System.Linq;
 
 namespace LibreriaDefinitivaFinaleNoMoreLibrerie.Controllers
 {
-    public class LibroController : BaseAPIController
+    public class LibroController : LibreriaDefinitiva.Controllers.BaseAPIController
     {
         private readonly ApplicationDbContext _db;
         private readonly ILogger<LibroController> _logger;

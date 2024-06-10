2a0ed23266f02ace07052537efd1fdaea6cfab5f
Luca Vezzoli <lucavezzoli9@gmail.com>
2024-06-10 09:44:21 +0200

    fetch searchBooks

--- LibreriaDefinitivaFinaleNoMoreLibrerie/Controllers/BaseAPIController.cs
+++ LibreriaDefinitivaFinaleNoMoreLibrerie/Controllers/BaseAPIController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace LibreriaDefinitiva.Controllers
+namespace LibreriaDefinitivaFinaleNoMoreLibrerie.Controllers
 {
     [ApiController]
     [Route("api/[controller]/[action]")]

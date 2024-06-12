d29add2b219bb5598d3f522dbd80c1534fb1b9be
Andrea Martino <andrea.martino@isisfacchinetti.ed.it>
2024-06-12 09:31:25 +0200

    commento sakr libroAperto

--- LibreriaDefinitivaFinaleNoMoreLibrerie/wwwroot/script/scriptLibroAperto.js
+++ LibreriaDefinitivaFinaleNoMoreLibrerie/wwwroot/script/scriptLibroAperto.js
@@ -1,15 +1,15 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
+﻿/*document.addEventListener('DOMContentLoaded', function () {
     let t = document.getElementById('immg');
     fetch('/api/LibroAPI')
         .then(response => response.json())
         .then(data => {
             data.forEach(b => {
                 let l = document.createElement('a');
-                l.href = pages / novita.html ? isbn = ${ b.isbn };  // Pass ISBN via query string
+                l.href = `pages/novita.html?isbn=${b.isbn}`;  // Pass ISBN via query string
                 let m = document.createElement('img');
-                m.src = ${ b.url };
+                m.src = `${b.url}`;
                 m.id = "g";
-                m.alt = ${ b.url };
+                m.alt = `${b.url}`;
                 l.appendChild(m);
                 t.appendChild(l);
 
@@ -22,3 +22,4 @@
         })
         .catch(error => console.error('Error fetching data:', error));
 });
+*/
